f48d5b73367e52e8cb514278238bc5af5c6ebe24
ArielCampos30 <153675296+ArielCampos30@users.noreply.github.com>
2026-09-24 22:23:37 -0300

    Corregir carga de estilos en Render

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'ServiceFlow')</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="/css/app.css?v=1">
 </head>
 <body>
 <div class="shell">
